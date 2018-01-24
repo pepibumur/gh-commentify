22d9fb41dffcbe148b0f5636564e6c61c914bd44
Pedro Piñera <pepibumur@gmail.com>
2018-01-24 18:48:38 -0500

    Clean up code and make it testable

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,47 @@
 // @ts-check
 import * as express from "express"
-const app = express()
-import fetch from "node-fetch"
+import fetch, { RequestInit, Response } from "node-fetch"
 
-app.set("port", process.env.PORT || 5000)
+interface GitHubGetFunction {
+  (path: string, request: RequestInit): Promise<Response>;
+}
 
-// e.g. https://api.github.com/repos/artsy/artsy.github.io/issues/355/comments
-app.get("/repos/:owner/:repo/issues/:number/comments", (req: any, res: any) => {
-  const gh = "https://api.github.com"
+interface RequestHandler {
+  (req: express.Request, res: Express.Response): void;
+}
+
+const githubGet: GitHubGetFunction = (path, request) => {
+  const ghUrl = "https://api.github.com"
+  return fetch([ghUrl, path].join("/"), request)
+}
+
+export const commentsHandler = (githubGet: GitHubGetFunction) => (req: express.Request, res: express.Response) => {
   if (req.params.owner !== process.env.ONLY_OWNER) {
     return res.status(401).json({ message: "Only can get comments with owner: " + process.env.ONLY_OWNER })
   }
-
-  const route = [gh, "repos", req.params.owner, req.params.repo, "issues", req.params.number, "comments"]
-  const url = route.join("/")
-
-
-  // https://developer.github.com/v3/#authentication
-  fetch(url, {
-    method: "GET",
-    headers: {
+  const path = ["repos", req.params.owner, req.params.repo, "issues", req.params.number, "comments"].join("/")
+  const request = {
+    method: "GET", headers: {
       Accept: "application/vnd.github.v3.html+json",
       Authorization: "token " + process.env.GITHUB_ACCESS_TOKEN,
-    },
+    }
+  }
+  githubGet(path, request).then(response => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    res.status(response.status)
+    const json = response.json()
+    res.json(json)
   })
-    .then(response => {
-      res.header("Access-Control-Allow-Origin", "*")
-      res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
-      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-      res.status(response.status)
-      return response.json()
-    })
-    .then(json => {
-      res.json(json)
-    })
-})
+}
 
-app.listen(app.get("port"), () => {
-  console.log(`Started server at http://localhost:${process.env.PORT || 5000}`)
-})
+export const appRunner = (app: express.Express = express(), handler: RequestHandler = commentsHandler(githubGet)) => {
+  app.set("port", process.env.PORT || 5000)
+  app.get("/repos/:owner/:repo/issues/:number/comments", handler)
+  app.listen(app.get("port"), () => {
+    console.log(`Started server at http://localhost:${process.env.PORT || 5000}`)
+  })
+}
+appRunner()
+
