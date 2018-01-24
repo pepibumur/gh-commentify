5cae2ab14eb167cc0c063e7ec94b2b4843be4a46
Pedro Piñera <pepibumur@gmail.com>
2018-01-24 18:22:14 -0500

    Migrate the implementation to Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 // @ts-check
-
-const express = require("express")
+import * as express from "express"
 const app = express()
-const fetch = require("node-fetch")
+import fetch from "node-fetch"
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
 app.set("port", process.env.PORT || 5000)
 
 // e.g. https://api.github.com/repos/artsy/artsy.github.io/issues/355/comments
-app.get("/repos/:owner/:repo/issues/:number/comments", (req, res) => {
+app.get("/repos/:owner/:repo/issues/:number/comments", (req: any, res: any) => {
   const gh = "https://api.github.com"
   if (req.params.owner !== process.env.ONLY_OWNER) {
     return res.status(401).json({ message: "Only can get comments with owner: " + process.env.ONLY_OWNER })
@@ -19,6 +18,7 @@ app.get("/repos/:owner/:repo/issues/:number/comments", (req, res) => {
 
   const route = [gh, "repos", req.params.owner, req.params.repo, "issues", req.params.number, "comments"]
   const url = route.join("/")
+
 
   // https://developer.github.com/v3/#authentication
   fetch(url, {
@@ -32,7 +32,6 @@ app.get("/repos/:owner/:repo/issues/:number/comments", (req, res) => {
       res.header("Access-Control-Allow-Origin", "*")
       res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
       res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
       res.status(response.status)
       return response.json()
     })
